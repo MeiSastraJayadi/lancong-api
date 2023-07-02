9fbaafae1887de11121d3d1424667aaec8298d78
MeiSastraJayadi <kadekmeisastra13@gmail.com>
2023-07-02 10:39:49 +0800

    create connection to database

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
+load_dotenv()
 
 @app.get("/")
 def hello() : 
     return "Hello World"
+
+url = os.getenv('DATABASE_URL')
+print(url)
+
